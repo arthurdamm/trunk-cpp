59215deef7d0d55c17ce82adfd98cf6c1b9cd51b
Arthur Damm <indigoarthur@gmail.com>
2021-12-19 10:09:51 -0800

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-	char *foo = (char *)malloc(100);
-	for (int i = 0; i < argc; i++) {
-		cout << argv[i] << endl;
+	
+	ifstream csv;
+	csv.open("test.csv");
+	string line = "";
+	while (getline(csv, line)) {
+		string name, tempString;
+		int id;
+		double gpa;
+
+		stringstream inputString(line);
+		getline(inputString, name, ',');
+		getline(inputString, tempString, ',');
+		id = atoi(tempString.c_str());
+		getline(inputString, tempString, ',');
+		gpa = atof(tempString.c_str());
+
+		printf(">> [%s] [%d] [%g]\n", name.c_str(), id, gpa);
+
 	}
+
 	return EXIT_SUCCESS;
 }
