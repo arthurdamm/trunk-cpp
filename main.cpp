970af803fa8032dbff21d2d95fee98c11037188c
Arthur Damm <indigoarthur@gmail.com>
2021-12-19 09:44:52 -0800

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,9 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	// char *foo = malloc(100);
+	char *foo = (char *)malloc(100);
 	for (int i = 0; i < argc; i++) {
-		// printf("[%s] ",  argv[i]);
 		cout << argv[i] << endl;
 	}
-	// printf("\n");
 	return EXIT_SUCCESS;
 }
